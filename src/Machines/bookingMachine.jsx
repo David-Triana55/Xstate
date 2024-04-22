409590fcfb79bcad8fd4397234f3355d35e65ce4
David Triana <davidtriana796@gmail.com>
2024-04-22 13:06:35 -0500

    first commit

---
+++ src/Machines/bookingMachine.jsx
@@ -0,0 +1,34 @@
+import { createMachine } from "xstate";
+
+const bookingMachine = createMachine({
+    id: "buy plane tickets",
+    initial: "initial",
+    states: {
+        initial: {
+            on: {
+                START: "search",
+            },
+        },
+        search: {
+            on: {
+                CONTINUE: "passengers",
+                CANCEL: "initial",
+            },
+        },
+        tickets: {
+            on: {
+                FINISH: "initial",
+                CANCEL: "initial",
+
+            },
+        },
+        passengers: {
+            on: {
+                DONE: "tickets",
+                CANCEL: "initial",
+            },
+        },
+    },
+});
+
+export default bookingMachine;
